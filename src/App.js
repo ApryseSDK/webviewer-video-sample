ae64ed0bc4a3bce8e0ca34aed2e494d9f8928568
CorreyL <correylim@gmail.com>
2020-06-24 13:58:35 -0700

    Remove signature tool button
    Per request from @andreysaf, as user's are unlikely to require making
    signatures in the frames of a video

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
           'viewControlsButton',
           'panToolButton',
           'textToolGroupButton',
+          'signatureToolButton',
         ],
       },
       viewer.current,
