9661d62f4cfdfa55c1bce6ef94bbe05d8c0251f6
CorreyL <correylim@gmail.com>
2020-06-26 10:45:08 -0700

    Support local dependency on WebViewer-video
    If `@pdftron/webviewer-video` is set as a local dependency using
    `file:$relative_path_to_webviewer-video`, then this application needs to
    be configured to ensure only one instance of `react` is being imported,
    otherwise we run into the issue as described here:
    
    https://reactjs.org/warnings/invalid-hook-call-warning.html#duplicate-react
    
    As such, create an alias for `react` in the dev `webpack` config file

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -150,6 +150,7 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      react: path.resolve(__dirname, '../node_modules/react'),
     },
     plugins: [
       // Adds support for installing with Plug'n'Play, leading to faster installs and adding
