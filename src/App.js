409a43186dcc3a020daea344a4175a3827fd575f
Kristian Hein <kristianhein@gmail.com>
2020-09-11 15:18:37 -0700

    progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import WebViewer from '@pdftron/webviewer';
 import { initializeVideoViewer, Controls } from '@pdftron/webviewer-video';
@@ -8,7 +8,6 @@ const DOCUMENT_ID = 'video';
 
 const App = () => {
   const viewer = useRef(null);
-  const [ documentLoaded, setDocumentLoaded ] = useState(false);
 
   // if using a class, equivalent of componentDidMount
   useEffect(() => {
@@ -38,7 +37,7 @@ const App = () => {
         ],
       },
       viewer.current,
-    ).then(async (instance) => {
+    ).then(async instance => {
       instance.setTheme('dark');
       instance.openElements('notesPanel');
 
@@ -46,7 +45,7 @@ const App = () => {
       // Extends WebViewer to allow loading HTML5 videos (.mp4, ogg, webm).
       const {
         loadVideo,
-       } = await initializeVideoViewer(
+      } = await initializeVideoViewer(
         instance,
         license,
       );
@@ -61,7 +60,7 @@ const App = () => {
       const annotManager = docViewer.getAnnotationManager();
 
       // Add save annotations button
-      setHeaderItems((header) => {
+      setHeaderItems(header => {
         header.push({
           type: 'actionButton',
           img: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
@@ -71,11 +70,11 @@ const App = () => {
 
             // Make a POST request with XFDF string
             const saveXfdfString = (documentId, xfdfString) => {
-              return new Promise((resolve) => {
+              return new Promise(resolve => {
                 fetch(`/server/annotationHandler.js?documentId=${documentId}`, {
                   method: 'POST',
                   body: xfdfString,
-                }).then((response) => {
+                }).then(response => {
                   if (response.status === 200) {
                     resolve();
                   }
@@ -85,7 +84,7 @@ const App = () => {
 
             const annotations = docViewer.getDocument().getVideo().getAllAnnotations();
             var xfdfString = await annotManager.exportAnnotations({ links: false, widgets: false, annotList: annotations });
-            await saveXfdfString(DOCUMENT_ID, xfdfString)
+            await saveXfdfString(DOCUMENT_ID, xfdfString);
             alert('Annotations saved successfully.');
           }
         });
@@ -96,15 +95,14 @@ const App = () => {
         const video = docViewer.getDocument().getVideo();
 
         // Make a GET request to get XFDF string
-        const loadXfdfString = (documentId) => {
-          return new Promise((resolve) => {
+        const loadXfdfString = documentId => {
+          return new Promise(resolve => {
             fetch(`/server/annotationHandler.js?documentId=${documentId}`, {
               method: 'GET'
-            }).then((response) => {
+            }).then(response => {
               if (response.status === 200) {
                 response.text()
-                  .then((xfdfString) => {
-                    console.log(xfdfString);
+                  .then(xfdfString => {
                     resolve(xfdfString);
                   });
               } else if (response.status === 204) {
@@ -120,14 +118,12 @@ const App = () => {
         };
 
         loadXfdfString(DOCUMENT_ID)
-          .then((xfdfString) => {
+          .then(xfdfString => {
             const annotManager = docViewer.getAnnotationManager();
-            return annotManager.importAnnotations(xfdfString)
+            return annotManager.importAnnotations(xfdfString);
           }).then(() => {
             video.updateAnnotationsToTime(0);
           });
-
-        setDocumentLoaded(true);
 
         const customContainer = instance.iframeWindow.document.querySelector('.custom-container');
         ReactDOM.render(
