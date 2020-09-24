db1c7c6ab3a5e8cab7777702ab00e933ed5750ca
Kristian Hein <kristianhein@gmail.com>
2020-09-24 14:09:47 -0700

    progress

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ const App = () => {
           'menuButton',
           'annotationCommentButton',
           'toggleNotesButton',
+          'annotationNoteConnectorLine',
         ],
       },
       viewer.current,
