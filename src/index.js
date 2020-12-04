1b0ee2fd8940b0c58da4b3e0f6a1e59626bf9641
Kristian Hein <kristianhein@gmail.com>
2020-12-04 14:25:30 -0800

    naming update

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import fetchInjectSwRegister from './sw-register';
+import registerFetchAuthInjection from './sw-register';
 
 // Only inject custom headers into videos from my endpoint
-// const myVideoEndPoint = 'https://www.my_video_endpoint.com/';
-const myVideoEndPoint = 'https://webviewer-video.s3.amazonaws.com';
+const myVideoEndPoint = 'https://www.my_video_endpoint.com/';
 const authenticationToken = 'Basic YWxhZGRpbjpvcGVuc2VzYW1l';
-fetchInjectSwRegister(myVideoEndPoint, authenticationToken);
+registerFetchAuthInjection(myVideoEndPoint, authenticationToken);
 
 navigator.serviceWorker.ready.then(() => {
   ReactDOM.render(<App />, document.getElementById('root'));
