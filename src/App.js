0a3e32b7688db43ca2344d1ed5d64acad1a47a92
Kristian Hein <kristianhein@gmail.com>
2021-01-11 23:14:11 -0800

    fix

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ const App = () => {
           'dropdown-item-status',
           'dropdown-item-author',
           'dropdown-item-type',
+          // 'zoomOverlayButton',
         ],
       },
       viewer.current,
@@ -67,7 +68,7 @@ const App = () => {
       const license = `---- Insert commercial license key here after purchase ----`;
       // Extends WebViewer to allow loading HTML5 videos (.mp4, ogg, webm).
       const {
-        loadVideo,
+        loadAudio,
       } = await initializeVideoViewer(
         instance,
         license,
@@ -77,41 +78,40 @@ const App = () => {
       // If local it needs to be relative to lib/ui/index.html.
       // Or at the root. (eg '/video.mp4')
       const videoUrl = 'https://pdftron.s3.amazonaws.com/downloads/pl/video/video.mp4';
-      loadVideo(videoUrl);
+      loadAudio(videoUrl);
 
-      const { docViewer, setHeaderItems } = instance;
-      const annotManager = docViewer.getAnnotationManager();
+      const { docViewer } = instance;
 
       // Add save annotations button
-      setHeaderItems(header => {
-        header.push({
-          type: 'actionButton',
-          img: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
-          onClick: async () => {
-            // Save annotations when button is clicked
-            // widgets and links will remain in the document without changing so it isn't necessary to export them
+      // setHeaderItems(header => {
+      //   header.push({
+      //     type: 'actionButton',
+      //     img: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
+      //     onClick: async () => {
+      //       // Save annotations when button is clicked
+      //       // widgets and links will remain in the document without changing so it isn't necessary to export them
 
-            // Make a POST request with XFDF string
-            const saveXfdfString = (documentId, xfdfString) => {
-              return new Promise(resolve => {
-                fetch(`/server/annotationHandler.js?documentId=${documentId}`, {
-                  method: 'POST',
-                  body: xfdfString,
-                }).then(response => {
-                  if (response.status === 200) {
-                    resolve();
-                  }
-                });
-              });
-            };
+      //       // Make a POST request with XFDF string
+      //       const saveXfdfString = (documentId, xfdfString) => {
+      //         return new Promise(resolve => {
+      //           fetch(`/server/annotationHandler.js?documentId=${documentId}`, {
+      //             method: 'POST',
+      //             body: xfdfString,
+      //           }).then(response => {
+      //             if (response.status === 200) {
+      //               resolve();
+      //             }
+      //           });
+      //         });
+      //       };
 
-            const annotations = docViewer.getAnnotationManager().getAnnotationsList();
-            var xfdfString = await annotManager.exportAnnotations({ links: false, widgets: false, annotList: annotations });
-            await saveXfdfString(DOCUMENT_ID, xfdfString);
-            alert('Annotations saved successfully.');
-          }
-        });
-      });
+      //       const annotations = docViewer.getAnnotationManager().getAnnotationsList();
+      //       var xfdfString = await annotManager.exportAnnotations({ links: false, widgets: false, annotList: annotations });
+      //       await saveXfdfString(DOCUMENT_ID, xfdfString);
+      //       alert('Annotations saved successfully.');
+      //     }
+      //   });
+      // });
 
       // Load saved annotations
       docViewer.on('documentLoaded', () => {
