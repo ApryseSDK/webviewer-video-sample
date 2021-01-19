bb7d2d545a1903b545c2f114169b8bc34d4b323c
Kristian Hein <kristianhein@gmail.com>
2021-01-18 22:00:22 -0800

    rewording

--- src/App.js
+++ src/App.js
@@ -67,15 +67,15 @@ const App = () => {
       !isSafari && instance.openElements('notesPanel');
 
       const license = `---- Insert commercial license key here after purchase ----`;
-      // Extends WebViewer to allow loading of media files.
+      // Extends WebViewer to allow loading media elements (.mp3, .mp4, ogg, webm, etc.)
       const {
         loadAudio,
       } = await initializeAudioViewer(
         instance,
-        license,
+        { license },
       );
 
-      // Load a file at a specific url. Can be a local or public link
+      // Load a media element at a specific url. Can be a local or public link
       // If local it needs to be relative to lib/ui/index.html.
       // Or at the root. (eg '/audio.mp3')
       const audioUrl = '/audio.mp3';
