c40611b9281911978d8907a8c4a892a4dce62c0a
Kristian Hein <kristianhein@gmail.com>
2021-01-21 16:41:49 -0800

    fix

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ const App = () => {
       // Extends WebViewer to allow loading HTML5 videos (.mp4, ogg, webm).
       const {
         loadVideo,
+        getFileULRWithMergedAnnots,
       } = await initializeVideoViewer(
         instance,
         license,
@@ -45,7 +46,7 @@ const App = () => {
       // Load a video at a specific url. Can be a local or public link
       // If local it needs to be relative to lib/ui/index.html.
       // Or at the root. (eg '/video.mp4')
-      const videoUrl = '/test.mp4';
+      const videoUrl = '/test4.mp4';
       loadVideo(videoUrl);
 
       const { docViewer, setHeaderItems } = instance;
@@ -87,6 +88,19 @@ const App = () => {
           img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 4H5c-1.11 0-2 .9-2 2v12c0 1.1.89 2 2 2h4v-2H5V8h14v10h-4v2h4c1.1 0 2-.9 2-2V6c0-1.1-.89-2-2-2zm-7 6l-4 4h3v6h2v-6h3l-4-4z"/></svg>',
           onClick: () => {
             inputFile.current.click();
+          }
+        });
+
+        header.push({
+          type: 'actionButton',
+          img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 4H5c-1.11 0-2 .9-2 2v12c0 1.1.89 2 2 2h4v-2H5V8h14v10h-4v2h4c1.1 0 2-.9 2-2V6c0-1.1-.89-2-2-2zm-7 6l-4 4h3v6h2v-6h3l-4-4z"/></svg>',
+          onClick: async () => {
+            const anchor = document.createElement('a');
+            anchor.href = await getFileULRWithMergedAnnots();
+            anchor.target = "_blank";
+            anchor.download = 'videoWithAnnots.mp4';
+            // Auto click on a element, trigger the file download
+            anchor.click();
           }
         });
       });
