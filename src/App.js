233170bc9de1e73af3895d836a32a791700074c6
Will Wang <wang.yw.william@gmail.com>
2021-03-08 23:56:16 -0800

    Adding ability to switch between video/pdf viewer seamlessly

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
   const viewer = useRef(null);
   const inputFile = useRef(null);
   const [ wvLoadVideo, setWvLoadVideo ] = useState(null);
+  const [ wvInstance, setInstance ] = useState(null);
   const license = `---- Insert commercial license key here after purchase ----`;
 
   // if using a class, equivalent of componentDidMount
@@ -23,6 +24,7 @@ const App = () => {
       viewer.current,
     ).then(async instance => {
       instance.openElements('notesPanel');
+      setInstance(instance);
 
       // Extends WebViewer to allow loading HTML5 videos (.mp4, ogg, webm).
       const {
@@ -96,7 +98,7 @@ const App = () => {
         header.push({
           type: 'actionButton',
           img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 4H5c-1.11 0-2 .9-2 2v12c0 1.1.89 2 2 2h4v-2H5V8h14v10h-4v2h4c1.1 0 2-.9 2-2V6c0-1.1-.89-2-2-2zm-7 6l-4 4h3v6h2v-6h3l-4-4z"/></svg>',
-          title: 'Load video file',
+          title: 'Load file',
           onClick: () => {
             inputFile.current.click();
           }
@@ -146,7 +148,22 @@ const App = () => {
 
   async function onFileChange(event) {
     const url = URL.createObjectURL(event.target.files[0]);
-    wvLoadVideo(url);
+
+    // Can also detect by specific video file types (ie. mp4, ogg, etc.)
+    if (event.target.files[0].type.includes('video')) {
+      await initializeVideoViewer(
+        wvInstance,
+        license,
+      );
+
+      setTimeout(() => {
+        wvInstance.openElements('notesPanel');
+      }, 500);
+
+      wvLoadVideo(url);
+    } else {
+      wvInstance.loadDocument(url);
+    }
   }
 
   return (
