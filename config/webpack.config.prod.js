f88487ed6e32fa293474c785bbe67b4601b8e191
Will Wang <wang.yw.william@gmail.com>
2021-05-04 18:32:42 -0700

    webpack

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -225,7 +225,8 @@ module.exports = {
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
       // Added by KRISTIAN for aliasing react
-      // react: path.resolve(__dirname, '../node_modules/react')
+      react: path.resolve(__dirname, '../node_modules/react'),
+      'react-dom': path.resolve(__dirname, '../node_modules/react-dom'),
     },
     plugins: [
       // Adds support for installing with Plug'n'Play, leading to faster installs and adding
