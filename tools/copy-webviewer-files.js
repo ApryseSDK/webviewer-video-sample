94f686eda30ffaf4e90dc69879a1b83d89219257
Kristian Hein <kristianhein@gmail.com>
2021-08-24 10:43:50 -0700

    service worker name change

--- tools/copy-webviewer-files.js
+++ tools/copy-webviewer-files.js
@@ -3,7 +3,7 @@ const fs = require('fs-extra');
 const copyFiles = async () => {
   try {
     await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer/lib');
-    await fs.copy('./node_modules/@pdftron/webviewer-video/src/lib/api/ServiceWorker/sw.js', './public/sw.js');
+    await fs.copy('./node_modules/@pdftron/webviewer-video/src/lib/api/ServiceWorker/wv-video-injection.js', './public/wv-video-injection.js');
     console.log('WebViewer files copied over successfully');
   } catch (err) {
     console.error(err);
