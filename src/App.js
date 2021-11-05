f11bbde430dd3bcca7f9f83d69881eb6009ba27a
Kristian Hein <kristianhein@gmail.com>
2021-11-05 00:59:09 -0700

    removed select annot on creation. Causes issues on mobile

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ const App = () => {
     WebViewer(
       {
         path: '/webviewer/lib',
-        selectAnnotationOnCreation: true,
       },
       viewer.current,
     ).then(async instance => {
