e90e9ccc0b2cd734fed561af3a5c6c63b7c05f95
Will Wang <wang.yw.william@gmail.com>
2022-01-17 11:43:25 -0800

    moving file to compare folder and updating webviewer copy script

--- tools/copy-webviewer-files.js
+++ tools/copy-webviewer-files.js
@@ -4,6 +4,7 @@ const copyFiles = async () => {
   try {
     await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer/lib');
     await fs.copy('./node_modules/@pdftron/webviewer-video/src/lib/api/ServiceWorker/wv-video-injection.js', './public/wv-video-injection.js');
+    await fs.copy('./src/compare/Compare.css', './public/Compare.css');
     fs.copySync('./node_modules/@pdftron/webviewer-video/ffprobe', './public/ffprobe');
     console.log('WebViewer files copied over successfully');
   } catch (err) {
